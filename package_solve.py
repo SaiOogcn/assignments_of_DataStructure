6274936af45feb932e1cbb9f2bf422a9e5b99195
Alvin Sun <zhichengsun6@gmail.com>
2025-12-18 23:26:47 +0800

    大作业2

---
+++ package_solve.py
@@ -0,0 +1,13 @@
+def neng_zhuang_man_bu(W, T):
+    a = [0] #用来装“可以通过W内数字相加达到的数字”的数组，初始为0，这是不证自明的
+    for i in W:
+        if i > T:
+            continue
+        d = [x + i for x in a] #为a中每个元素都加上本次遍历到的数字
+        a = a + d #把新数组与旧数组合并
+        if T in a:
+            print(f"体积{T}刚好能被填满")
+            return True
+
+    print(f"体积{T}没法被{W}刚好填满")
+    return False
